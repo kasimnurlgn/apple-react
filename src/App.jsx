48d77fe22b12b71ceff58e3069ee4b5fe3611550
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-04-14 14:08:23 +0300

    FirstSection added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <Header />
       <Alert />
-      {/* <FirstSection /> */}
+      <FirstSection />
       {/* <SecondSection /> */}
       {/* <ThirdSection /> */}
       {/* <FourthSection /> */}
